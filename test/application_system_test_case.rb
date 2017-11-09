7fcf2d6bc7e1aca5b25e261dac82b56881923881
Doug Stull <dstull@Dougs-MBP.fios-router.home>
2017-11-08 22:25:00 -0500

    added comments to the test case setup for chrome options

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -4,7 +4,20 @@ require 'capybara'
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
 
+
   Capybara.register_driver :headless_chrome do |app|
+
+    # options explained https://peter.sh/experiments/chromium-command-line-switches/
+    # no-sandbox
+    #   because the user namespace is not enabled in the container by default
+    # headless
+    #   run w/o actually launching gui
+    # disable-gpu
+    #   Disables graphics processing unit(GPU) hardware acceleration
+    # window-size
+    #   sets default window size in case the smaller default size is not enough
+    #   we do not want max either, so this is a good compromise
+    
     capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
         chromeOptions: { args: %w[no-sandbox headless disable-gpu window-size=1400,1400] }
     )
